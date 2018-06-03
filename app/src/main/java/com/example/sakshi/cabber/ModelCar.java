cee21530056afd9e085559f2950f15b616fb043e
Kristen_28 <sakshichahal53@gmail.com>
2018-06-03 13:58:30 +0530

    google map added

---
+++ app/src/main/java/com/example/sakshi/cabber/ModelCar.java
@@ -0,0 +1,50 @@
+package com.example.sakshi.cabber;
+
+/**
+ * Created by sakshi on 3/6/18.
+ */
+
+public class ModelCar {
+
+    private String name, lower_value, uppervalue;
+    private int image;
+
+    public ModelCar(String name, String lower_value, String uppervalue, int image) {
+        this.name = name;
+        this.lower_value = lower_value;
+        this.uppervalue = uppervalue;
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLower_value() {
+        return lower_value;
+    }
+
+    public void setLower_value(String lower_value) {
+        this.lower_value = lower_value;
+    }
+
+    public String getUppervalue() {
+        return uppervalue;
+    }
+
+    public void setUppervalue(String uppervalue) {
+        this.uppervalue = uppervalue;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+}
