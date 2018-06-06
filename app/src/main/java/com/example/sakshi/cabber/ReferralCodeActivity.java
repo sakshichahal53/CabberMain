806e4ad6b529d477f06f7001f72d930e59a0d809
Sakshi Chahal <sakshichahal53@gmail.com>
2018-06-06 12:59:32 +0530

    7 layout where to go added

---
+++ app/src/main/java/com/example/sakshi/cabber/ReferralCodeActivity.java
@@ -0,0 +1,13 @@
+package com.example.sakshi.cabber;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReferralCodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_referral_code);
+    }
+}
---
+++ app/src/main/java/com/example/sakshi/cabber/WhereToGoActivity.java
@@ -0,0 +1,13 @@
+package com.example.sakshi.cabber;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WhereToGoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_where_to_go);
+    }
+}
